050ba759772a0163b3cb01dfdb18542e3007f7b6
LongHN <huynhngoclong917@gmail.com>
2026-01-28 10:26:34 +0700

    Khởi tạo Project

---
+++ server/src/main/java/com/fpsgame/server/FpsgameServerApplication.java
@@ -0,0 +1,13 @@
+package com.fpsgame.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpsgameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpsgameServerApplication.class, args);
+	}
+
+}
